3ba098f21e9bab5ef5b2df071f823c064457b02f
winminhtetz <mgwinnminnhtet111@gmail.com>
2022-02-14 12:53:15 +0630

    First Commit

---
+++ app/src/main/java/com/wmh/firebaseui/MainActivity.java
@@ -0,0 +1,76 @@
+package com.wmh.firebaseui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int AUTH_CODE = 118;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    private List<AuthUI.IdpConfig> providers;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if (listener != null) firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        TextView textView = findViewById(R.id.tv_currentUser);
+        textView.setText("User : " + user);
+
+    }
+
+    private void init() {
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+                //new AuthUI.IdpConfig.FacebookBuilder().build()
+        );
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        listener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, "You are already sign in", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers).
+                                    build(), AUTH_CODE);
+                }
+            }
+        };
+
+    }
+}
